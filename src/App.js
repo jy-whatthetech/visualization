386c8602e736302f82a006f600f58171e4b1d019
jy-whatthetech <james.yeh1@gmail.com>
2020-08-26 23:47:57 -0700

    add demo graph

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BarGraph } from "./Demo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={logo} className="App-logo" alt="logo" />
-          Learn React: Hello World
-        </a>
+        <BarGraph />
       </header>
     </div>
   );
---
+++ src/Demo.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { letterFrequency } from '@vx/mock-data';
+import { Group } from '@vx/group';
+import { Bar } from '@vx/shape';
+import { scaleLinear, scaleBand } from '@vx/scale';
+
+// We'll use some mock data from `@vx/mock-data` for this.
+const data = letterFrequency;
+
+// Define the graph dimensions and margins
+const width = 500;
+const height = 500;
+const margin = { top: 20, bottom: 20, left: 20, right: 20 };
+
+// Then we'll create some bounds
+const xMax = width - margin.left - margin.right;
+const yMax = height - margin.top - margin.bottom;
+
+// We'll make some helpers to get at the data we want
+const x = d => d.letter;
+const y = d => +d.frequency * 100;
+
+// And then scale the graph by our data
+const xScale = scaleBand({
+  range: [0, xMax],
+  round: true,
+  domain: data.map(x),
+  padding: 0.4,
+});
+const yScale = scaleLinear({
+  range: [yMax, 0],
+  round: true,
+  domain: [0, Math.max(...data.map(y))],
+});
+
+// Compose together the scale and accessor functions to get point functions
+const compose = (scale, accessor) => data => scale(accessor(data));
+const xPoint = compose(xScale, x);
+const yPoint = compose(yScale, y);
+
+// Finally we'll embed it all in an SVG
+export function BarGraph(props) {
+  return (
+    <svg width={width} height={height}>
+      {data.map((d, i) => {
+        const barHeight = yMax - yPoint(d);
+        return (
+          <Group key={`bar-${i}`}>
+            <Bar
+              x={xPoint(d)}
+              y={yMax - barHeight}
+              height={barHeight}
+              width={xScale.bandwidth()}
+              fill="#fc2e1c"
+            />
+          </Group>
+        );
+      })}
+    </svg>
+  );
+}
+
+// ... somewhere else, render it ...
+// <BarGraph />
