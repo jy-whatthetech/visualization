15cab7ecd1937a89b971612beca23a6d4e021df8
jy-whatthetech <james.yeh1@gmail.com>
2020-08-29 23:36:53 -0700

    read user input

--- src/App.js
+++ src/App.js
@@ -3,15 +3,36 @@ import logo from "./logo.svg";
 import "./App.css";
 import { default as Example } from "./Network";
 
+function parseInputValue(text) {
+  //Do something to update node state
+}
+
 function App() {
+  const [inputValue, setInputValue] = React.useState("placeholder text");
+
+  React.useEffect(() => {
+    //TODO:
+    console.log("input value changed");
+  }, [inputValue]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Example width={500} height={500}/>
-      </header>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <form action="/action_page.php">
+        <label for="fname">First name:</label>
+        <input
+          type="text"
+          id="fname"
+          name="fname"
+          value={inputValue}
+          onChange={event => {
+            setInputValue(event.target.value);
+          }}
+        />
+      </form>
+      <Example width={500} height={500} />
     </div>
   );
 }
