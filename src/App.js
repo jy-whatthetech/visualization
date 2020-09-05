e22442c238488e6241e69ad2802a03cc0def1463
jy-whatthetech <james.yeh1@gmail.com>
2020-09-04 23:50:52 -0700

    switch to react-d3-graph, render nodes with static positions

--- src/App.js
+++ src/App.js
@@ -1,40 +1,99 @@
 import React from "react";
 import "./App.css";
-import { default as Example } from "./Network";
+import { Graph } from "react-d3-graph";
 
 function App() {
-  const [inputValue, setInputValue] = React.useState("placeholder text");
-  const [numNodes, setNumNodes] = React.useState(3);
+  // graph payload (with minimalist structure)
+  const data = {
+    nodes: [
+      { id: "Harry", x: 50, y: 50 },
+      { id: "Sally", x: 100, y: 100 },
+      { id: "Alice", x: 200, y: 200 }
+    ],
+    links: [
+      { source: "Harry", target: "Sally" },
+      { source: "Harry", target: "Alice" }
+    ]
+  };
 
-  React.useEffect(() => {
-    //TODO:
-    const parsed = parseInt(inputValue, 10);
-    if (!isNaN(parsed)) {
-      setNumNodes(parsed);
+  // the graph configuration, you only need to pass down properties
+  // that you want to override, otherwise default ones will be used
+  const myConfig = {
+    nodeHighlightBehavior: true,
+    staticGraphWithDragAndDrop: true,
+    node: {
+      color: "lightgreen",
+      size: 120,
+      highlightStrokeColor: "blue"
+    },
+    link: {
+      highlightColor: "lightblue"
     }
+  };
 
-    console.log("input value changed");
-  }, [inputValue]);
+  // graph event callbacks
+  const onClickGraph = function() {
+    window.alert(`Clicked the graph background`);
+  };
 
+  const onClickNode = function(nodeId) {
+    window.alert(`Clicked node ${nodeId}`);
+  };
+
+  const onDoubleClickNode = function(nodeId) {
+    window.alert(`Double clicked node ${nodeId}`);
+  };
+
+  const onRightClickNode = function(event, nodeId) {
+    window.alert(`Right clicked node ${nodeId}`);
+  };
+
+  const onMouseOverNode = function(nodeId) {
+    window.alert(`Mouse over node ${nodeId}`);
+  };
+
+  const onMouseOutNode = function(nodeId) {
+    window.alert(`Mouse out node ${nodeId}`);
+  };
+
+  const onClickLink = function(source, target) {
+    window.alert(`Clicked link between ${source} and ${target}`);
+  };
+
+  const onRightClickLink = function(event, source, target) {
+    window.alert(`Right clicked link between ${source} and ${target}`);
+  };
+
+  const onMouseOverLink = function(source, target) {
+    window.alert(`Mouse over in link between ${source} and ${target}`);
+  };
+
+  const onMouseOutLink = function(source, target) {
+    window.alert(`Mouse out link between ${source} and ${target}`);
+  };
+
+  const onNodePositionChange = function(nodeId, x, y) {
+    window.alert(
+      `Node ${nodeId} is moved to new position. New position is x= ${x} y= ${y}`
+    );
+  };
   return (
-    <div className="App">
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <form action="/action_page.php">
-        <label for="fname">First name:</label>
-        <input
-          type="text"
-          id="fname"
-          name="fname"
-          value={inputValue}
-          onChange={event => {
-            setInputValue(event.target.value);
-          }}
-        />
-      </form>
-      <Example width={500} height={500} numNodes={numNodes} />
-    </div>
+    <Graph
+      id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
+      data={data}
+      config={myConfig}
+      // onClickNode={onClickNode}
+      // onDoubleClickNode={onDoubleClickNode}
+      // onRightClickNode={onRightClickNode}
+      // onClickGraph={onClickGraph}
+      // onClickLink={onClickLink}
+      // onRightClickLink={onRightClickLink}
+      // onMouseOverNode={onMouseOverNode}
+      // onMouseOutNode={onMouseOutNode}
+      // onMouseOverLink={onMouseOverLink}
+      // onMouseOutLink={onMouseOutLink}
+      onNodePositionChange={onNodePositionChange}
+    />
   );
 }
 
