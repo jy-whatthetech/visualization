e1dc4199df99edce5ffe80e95031712e2cbe5ae4
jy-whatthetech <james.yeh1@gmail.com>
2020-09-09 16:18:13 -0700

    parse and display user input, with random x and y positions

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,12 @@ import React from "react";
 import "./App.css";
 import Graph from "./graph/Graph";
 import * as ParseUtils from "./parser/parseUtils";
+import * as Utils from "./utils/utils";
 
 function App() {
   const [inputValue, setInputValue] = React.useState("");
-
-  React.useEffect(() => {
-    //TODO:
-    console.log("input value changed");
-    console.log(inputValue);
-
-    const parsedValue = ParseUtils.parsePairs(inputValue);
-
-    console.log("parsed value");
-    console.log(parsedValue);
-  }, [inputValue]);
-
   // graph payload (with minimalist structure)
-  const data = {
+  const [data, setData] = React.useState({
     nodes: [
       { id: "Harry", x: 50, y: 50 },
       { id: "Sally", x: 100, y: 200 },
@@ -28,20 +17,47 @@ function App() {
       { source: "Harry", target: "Sally" },
       { source: "Harry", target: "Alice" }
     ]
-  };
+  });
+
+  React.useEffect(() => {
+    if (!inputValue) return;
+    console.log("input value changed");
+    console.log(inputValue);
+
+    // TODO: ERROR HANDLING
+    const parsedValue = ParseUtils.parsePairs(inputValue);
+
+    console.log("parsed value");
+    console.log(parsedValue);
+
+    const tempNodes: Array<any> = [];
+
+    for (let nodeId of Array.from(parsedValue.nodeSet)) {
+      let x = Utils.randomInRange(10, 800);
+      let y = Utils.randomInRange(10, 600);
+      tempNodes.push({ id: nodeId, x: x, y: y });
+    }
+
+    parsedValue.nodes = tempNodes;
+    console.log(parsedValue);
+    setData(parsedValue);
+  }, [inputValue]);
 
   return (
     <>
+      [[2,1],[3,1],[1,4]]
       <form>
-        <label>Graph Input:</label>
-        <textarea
-          id="graph-input"
-          name="graph-input"
-          value={inputValue}
-          onChange={event => {
-            setInputValue(event.target.value);
-          }}
-        />
+        <label>
+          Graph Input:
+          <textarea
+            id="graph-input"
+            name="graph-input"
+            value={inputValue}
+            onChange={event => {
+              setInputValue(event.target.value);
+            }}
+          />
+        </label>
       </form>
       <Graph
         id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
--- src/parser/parseUtils.ts
+++ src/parser/parseUtils.ts
@@ -1,11 +1,41 @@
 // directed pairs
 // [[2,1],[3,1],[1,4]]
-export function parsePairs(input: string, directed = true) {
+export function parsePairs(input: string, directed = true): any {
   // trim whitespace
+  // TODO: ERROR HANDLING
   input = input.trim();
   input = input.slice(1, input.length - 1);
 
-  return input;
+  const links = [];
+  const nodeSet = new Set<string>();
+
+  let startInd = 0;
+  let nextOpenBracket = input.indexOf("[", startInd);
+  while (nextOpenBracket !== -1) {
+    const nextCloseBracket = input.indexOf("]", nextOpenBracket);
+    const pair = getDirectedPair(
+      input.slice(nextOpenBracket + 1, nextCloseBracket),
+      nodeSet
+    );
+    links.push(pair);
+
+    startInd = nextCloseBracket;
+    nextOpenBracket = input.indexOf("[", startInd);
+  }
+
+  return { nodeSet: nodeSet, links: links };
+}
+
+function getDirectedPair(s: string, nodeSet: Set<string>) {
+  s = s.trim();
+  const sp = s.split(",");
+  const src = sp[0].trim();
+  const trg = sp[1].trim();
+
+  nodeSet.add(src);
+  nodeSet.add(trg);
+
+  return { source: src, target: trg };
 }
 
 // adjacency array
