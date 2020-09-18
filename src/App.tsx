c74503a860b3c6822260cba0bda651c5562ac677
jy-whatthetech <james.yeh1@gmail.com>
2020-09-17 23:56:07 -0700

    mock up layout with Drawer component from Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from "react";
+import clsx from "clsx";
 import "./App.css";
 import Graph from "./graph/Graph";
 import * as ParseUtils from "./parser/parseUtils";
 import * as Utils from "./utils/utils";
 import { InputType, getLabel } from "./parser/inputTypes";
-import { FormControl, MenuItem, InputLabel, Select } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles(theme => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 220
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2)
-  }
-}));
+import {
+  FormControl,
+  MenuItem,
+  InputLabel,
+  Select,
+  AppBar,
+  Drawer,
+  Toolbar,
+  Divider,
+  IconButton,
+  Typography
+} from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { useStyles } from "./styles/useStyles";
+import {
+  Menu as MenuIcon,
+  ChevronLeft as ChevronLeftIcon
+} from "@material-ui/icons";
 
 function App() {
   const classes = useStyles();
+
+  // layout
+  const [drawerOpen, setDrawerOpen] = React.useState(true);
+
+  // input data
   const [inputValue, setInputValue] = React.useState("");
   const [comboValue, setComboValue] = React.useState(0);
   const [directed, setDirected] = React.useState(true);
@@ -62,73 +74,126 @@ function App() {
   }, [inputValue]);
 
   return (
-    <>
-      <FormControl className={classes.formControl}>
-        <InputLabel id="graph-input-type-label">Input Type</InputLabel>
-        <Select
-          labelId="graph-input-type-label"
-          id="graph-input-type"
-          value={comboValue}
-          className={classes.selectEmpty}
-          onChange={e => {
-            setComboValue(parseInt(e.target.value as string));
-          }}
-        >
-          {Object.keys(InputType)
-            .filter(k => typeof InputType[k as any] !== "number")
-            .map(key => (
-              <MenuItem key={key} value={key}>
-                {getLabel(parseInt(key))}
-              </MenuItem>
-            ))}
-        </Select>
-      </FormControl>
-      [[2,1],[3,1],[1,4]]
-      <br />
-      [[1],[],[0,5],[],[1,3,0],[0]]
-      <br />
-      <form>
-        <label>
-          Directed:{" "}
-          <input
-            type="checkbox"
-            value={"directedValue"}
-            checked={directed}
-            onChange={e => setDirected(!directed)}
-          />
-        </label>
-        <br />
-        <label>
-          Graph Input:
-          <textarea
-            id="graph-input"
-            name="graph-input"
-            value={inputValue}
-            onChange={event => {
-              setInputValue(event.target.value);
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: drawerOpen
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() => {
+              setDrawerOpen(true);
             }}
-          />
-        </label>
-        <label>
-          Custom Nodes:
-          <textarea
-            id="custom-nodes"
-            name="custom-nodes"
-            value={customNodes}
-            onChange={event => {
-              setCustomNodes(event.target.value);
+            edge="start"
+            className={clsx(classes.menuButton, drawerOpen && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Persistent drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={drawerOpen}
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton
+            onClick={() => {
+              setDrawerOpen(false);
             }}
-          />
-        </label>
-      </form>
-      <Graph
-        id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
-        inputType={comboValue}
-        directed={directed}
-        customNodes={customNodes}
-        data={data}
-      />
-    </>
+          >
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <div>
+          <Divider />
+          [[2,1],[3,1],[1,4]]
+          <br />
+          [[1],[],[0,5],[],[1,3,0],[0]]
+          <br />
+          <FormControl className={classes.formControl}>
+            <InputLabel id="graph-input-type-label">Input Type</InputLabel>
+            <Select
+              labelId="graph-input-type-label"
+              id="graph-input-type"
+              value={""}
+              className={classes.selectEmpty}
+              onChange={e => {
+                setComboValue(parseInt(e.target.value as string));
+              }}
+            >
+              {Object.keys(InputType)
+                .filter(k => typeof InputType[k as any] !== "number")
+                .map(key => (
+                  <MenuItem key={key} value={key}>
+                    {getLabel(parseInt(key))}
+                  </MenuItem>
+                ))}
+            </Select>
+          </FormControl>
+          <form>
+            <label>
+              Directed:{" "}
+              <input
+                type="checkbox"
+                value={"directedValue"}
+                checked={directed}
+                onChange={e => setDirected(!directed)}
+              />
+            </label>
+            <br />
+            <label>
+              Graph Input:
+              <textarea
+                id="graph-input"
+                name="graph-input"
+                value={inputValue}
+                onChange={event => {
+                  setInputValue(event.target.value);
+                }}
+              />
+            </label>
+            <label>
+              Custom Nodes:
+              <textarea
+                id="custom-nodes"
+                name="custom-nodes"
+                value={customNodes}
+                onChange={event => {
+                  setCustomNodes(event.target.value);
+                }}
+              />
+            </label>
+          </form>
+        </div>
+      </Drawer>
+      <main
+        className={clsx(classes.mainContent, {
+          [classes.contentShift]: drawerOpen
+        })}
+      >
+        <div className={classes.drawerHeader} />
+        <Graph
+          id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
+          inputType={comboValue}
+          directed={directed}
+          customNodes={customNodes}
+          data={data}
+        />
+      </main>
+    </div>
   );
 }
 
---
+++ src/styles/useStyles.ts
@@ -0,0 +1,71 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const drawerWidth = 240;
+const formWidth = 220;
+
+export const useStyles = makeStyles(theme => ({
+  // layout styles
+  root: {
+    display: "flex"
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end"
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  hide: {
+    display: "none"
+  },
+  mainContent: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    marginLeft: -drawerWidth
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    marginLeft: 0
+  },
+
+  // form styles
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: formWidth
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2)
+  }
+}));
